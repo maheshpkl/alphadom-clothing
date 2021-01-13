2eb80897ce7f9bba2be41f43d57c5f97dc66c1ce
maheshpkl <mahesh.tgi213@gmail.com>
2021-01-13 22:04:46 +0630

    adding input components and custom button to the sign-in component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/header.component';
-
 import Homepage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
