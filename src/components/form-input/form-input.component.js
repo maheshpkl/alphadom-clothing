9d1d457e414618f4f76d79a5deb9a9d426f6494c
maheshpkl <mahesh.tgi213@gmail.com>
2021-01-15 16:29:59 +0630

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -8,7 +8,7 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
     {label ? (
       <label
         className={`${
-          otherProps.value.length ? 'shirnk' : ''
+          otherProps.value.length ? 'shrink' : ''
         } form-input-label`}
       >
         {label}
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   appId: '1:21943602271:web:eff5aca8e70906fbfedfea'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRaf = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRaf.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRaf.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('Error creating user', error.message);
+    }
+  }
+  return userRaf;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
